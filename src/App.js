84a21b07d7f4be4d647854f218a0cca3c181cc44
kkasperek <kt.kasperek@gmail.com>
2020-08-12 16:45:11 -0500

    added auth and amplify

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { withAuthenticator } from 'aws-amplify-react'
+import { withAuthenticator } from 'aws-amplify-react';
 import Amplify, { Auth } from 'aws-amplify';
+import logo from './assets/logo.svg';
 import aws_exports from './aws-exports';
+
 Amplify.configure(aws_exports);
 
 class App extends Component {
@@ -13,7 +13,11 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit
+            {' '}
+            <code>src/App.js</code>
+            {' '}
+            and save to reload.
           </p>
           <a
             className="App-link"
